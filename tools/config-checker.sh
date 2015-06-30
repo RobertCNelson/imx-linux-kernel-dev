03b3db116b56432ad019aaa00a49eee1690548f3
Robert Nelson <robertcnelson@gmail.com>
2015-06-30 15:05:37 -0500

    scripts: sync with master of: https://github.com/RobertCNelson/stable-kernel.git
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ tools/config-checker.sh
@@ -0,0 +1,49 @@
+#!/bin/sh -e
+
+DIR=$PWD
+
+config_enable () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "xy" ] ; then
+		echo "Setting: ${config}=y"
+		./scripts/config --enable ${config}
+	fi
+}
+
+config_disable () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "xn" ] ; then
+		echo "Setting: ${config}=n"
+		./scripts/config --disable ${config}
+	fi
+}
+
+config_module () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "xm" ] ; then
+		echo "Setting: ${config}=m"
+		./scripts/config --module ${config}
+	fi
+}
+
+config_string () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "x${option}" ] ; then
+		echo "Setting: ${config}=${option}"
+		./scripts/config --set-str ${config} ${option}
+	fi
+}
+
+config_value () {
+	ret=$(./scripts/config --state ${config})
+	if [ ! "x${ret}" = "x${option}" ] ; then
+		echo "Setting: ${config}=${option}"
+		./scripts/config --set-val ${config} ${option}
+	fi
+}
+
+cd ${DIR}/KERNEL/
+
+cd ${DIR}/
+
+#
